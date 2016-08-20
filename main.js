fc5924b3d440f63f08431176cffdfed1ef39fc16
Dani Adkins <danirenaeadkins12@gmail.com>
2016-08-20 10:09:01 -0500

    class work

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-console.log("main.js");
+
+
+console.log("Sedan IIFE", Sedan);
+console.log("Sedan Color", Sedan.getColor());
+console.log("Sedan type", Sedan.getType());
+
+console.log("Sedan set Occupancy", Sedan.setOccupancy(5));   // main is anything I want to do to DOM ; click events, class changes, etc.
+console.log("Sedan get Occupancy", Sedan.getOccupancy());
